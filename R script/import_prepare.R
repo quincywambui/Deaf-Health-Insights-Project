9b0cf696dae528f51a3ae7a8704f15d0de3fe09b
quincywambui <quuncywambui7@gmail.com>
2024-10-19 19:52:53 +0100

    added file path to data and loaded it

---
+++ R script/import_prepare.R
@@ -0,0 +1,7 @@
+# Data Processing Script
+
+#### 1.0  Read the data -------
+df0_deaf <- readxl::read_xlsx("\\Users\\Quincy Wambui\\Desktop\\_GitRepos\\Data\\Deaf Community Research Nigeria Data for Viz for Social Good V.2.xlsx")
+
+#### 1.1  Check the data -------
+df0_deaf
